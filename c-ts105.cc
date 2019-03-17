10dd44926e8a12af0073feb046ab22570a94fd14
Kensuke Yamamoto <yamamoto-kensuke-ph@ynu.jp>
2019-03-17 22:37:05 +0900

    Update c-ts105.cc to count # of hits in each channel correctly

--- c-ts105.cc
+++ c-ts105.cc
@@ -123,7 +123,12 @@ int main(int argc, char *argv[]){
 			for(i=0;i<NCHTDC;i++){
 				tdchits[i] = 0;
 				CAMAC(NAF(TDC_STATION, TDCChannels[i], N_HITS), &tdchits[i], &q, &x);
-				Nofhits[i] = tdchits[i] - 0xff0000;
+				if(TDCChannels[i]%2==0){
+					Nofhits[i] = tdchits[i] - 0xff0000;
+				}
+				else{
+					Nofhits[i] = tdchits[i] - 0xff00ff;
+				}
 				if(Nofhits[i] != 1){
 					readdata++;
 				}
@@ -157,7 +162,12 @@ int main(int argc, char *argv[]){
 			for(i=0;i<NCHTDC;i++){
 				tdchits[i] = 0;
 				CAMAC(NAF(TDC_STATION, TDCChannels[i], N_HITS), &tdchits[i], &q, &x);
-				Nofhits[i] = tdchits[i] - 0xff0000;
+				if(TDCChannels[i]%2==0){
+					Nofhits[i] = tdchits[i] - 0xff0000;
+				}
+				else{
+					Nofhits[i] = tdchits[i] - 0xff00ff;
+				}
 				for(j=0;j<Nofhits[i];j++){
 					CAMAC(NAF(TDC_STATION, TDCChannels[i], READ_TDC), &tdcdata[i], &q, &x);
 					printf("TDC Ch%d value %5d / ",TDCChannels[i],tdcdata[i]);
